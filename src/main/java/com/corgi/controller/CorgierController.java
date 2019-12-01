b5854a21f5b622eddd5f221bd45c08702422a4fc
tairanliu <dasltr@126.com>
2019-12-02 01:14:59 +0800

    init

---
+++ src/main/java/com/corgi/controller/CorgierController.java
@@ -0,0 +1,27 @@
+package com.corgi.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.corgi.common.JsonResult;
+import com.corgi.user.api.profile.service.CorgierProfileService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author tairanliu
+ */
+@Slf4j
+@RestController
+@RequestMapping("user")
+public class CorgierController extends BaseController {
+    @Reference(version = "1.0.0")
+    private CorgierProfileService corgierProfileService;
+
+    @GetMapping("/regist")
+    public JsonResult regist() {
+        corgierProfileService.register();
+        return new JsonResult(null);
+    }
+
+}
