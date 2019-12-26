03220a924899021d637bc5d6f179830132e4b28b
tairanliu <dasltr@126.com>
2019-12-26 10:05:04 +0800

    添加活动相关接口

--- src/main/java/com/corgi/controller/CorgiActivityController.java
+++ src/main/java/com/corgi/controller/CorgiActivityController.java
@@ -14,9 +14,7 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author tairanliu
@@ -39,6 +37,41 @@ public class CorgiActivityController extends BaseController {
     public JsonResult addActivity(@RequestBody CorgiActivity activity) {
         activity = corgiActivityService.addCorgiActivity(activity);
         return new JsonResult(activity);
+    }
+
+    @GetMapping("test_add_activity")
+    public JsonResult testAddActivity() {
+        CorgiActivity activity = new CorgiActivity();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+        Random random = new Random();
+        activity.setCreateTime(sdf.format(new Date()));
+        activity.setStatus(CorgiActivity.CREATED);
+        activity.setLat(random.nextDouble());
+        activity.setLng(random.nextDouble());
+        activity.setActivityType("test");
+        activity.setAddress("阿维机构啊叫，给皮卡金额");
+        activity.setBudget(124);
+        activity.setContent("阿我诶咕叽咕叽哦可刺激噶我");
+        activity.setPayType("AA");
+        activity.setTitle("测试");
+        activity.setPeopleCount(341);
+        activity.setSignUpTime("2020/11/11 00:00");
+        activity = corgiActivityService.addCorgiActivity(activity);
+        activity.setTitle("测试33");
+        corgiActivityService.updateCorgiActivity(activity);
+        return new JsonResult(activity);
+    }
+
+    @PostMapping("update_activity")
+    public JsonResult updateActivity(@RequestBody CorgiActivity activity) {
+        activity = corgiActivityService.updateCorgiActivity(activity);
+        return new JsonResult(activity);
+    }
+
+    @GetMapping("delete_activity")
+    public JsonResult deleteActivity(String activityId) {
+        corgiActivityService.deleteCorgiActivity(activityId);
+        return new JsonResult();
     }
 
     @GetMapping("sign_up")
