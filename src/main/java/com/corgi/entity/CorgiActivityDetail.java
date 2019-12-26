b007b91b5cd658df6c676077f0f5bf550c7bf7fe
tairanliu <dasltr@126.com>
2019-12-26 11:38:28 +0800

    fix bug

--- src/main/java/com/corgi/entity/CorgiActivityDetail.java
+++ src/main/java/com/corgi/entity/CorgiActivityDetail.java
@@ -2,6 +2,7 @@ package com.corgi.entity;
 
 import com.corgi.activity.entity.CorgiActivity;
 import lombok.Data;
+import org.springframework.beans.BeanUtils;
 
 /**
  * @author tairanliu
@@ -15,19 +16,7 @@ public class CorgiActivityDetail extends CorgiActivity {
     }
 
     public CorgiActivityDetail(CorgiActivity activity) {
-        setActivityType(activity.getActivityType());
-        setAddress(activity.getAddress());
-        setBudget(activity.getBudget());
-        setContent(activity.getContent());
-        setSignUpTime(activity.getSignUpTime());
-        setId(activity.getId());
-        setLat(activity.getLat());
-        setLng(activity.getLng());
-        setPayType(activity.getPayType());
-        setPeopleCount(activity.getPeopleCount());
-        setPics(activity.getPics());
-        setTitle(activity.getTitle());
-        setUserId(activity.getUserId());
+        BeanUtils.copyProperties(activity, this);
     }
 
     public CorgiActivityDetail initMatch(double match) {
