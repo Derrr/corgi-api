2c7b1fdc90ccc9ec9671c7f4de6b453fd9425aff
tairanliu <dasltr@126.com>
2020-01-24 18:17:10 +0800

    添加默认参数

--- src/main/java/com/corgi/controller/CorgiActivityController.java
+++ src/main/java/com/corgi/controller/CorgiActivityController.java
@@ -197,7 +197,7 @@ public class CorgiActivityController extends BaseController {
     }
 
     @GetMapping("get_range_activity")
-    public JsonResult getRangeActivity(@RequestParam("userId") String userId, @RequestParam(name = "lng", required = false) double lng, @RequestParam(name = "lat", required = false) double lat, @RequestParam(name = "range", required = false) double range, ActivityQuery activityQuery) {
+    public JsonResult getRangeActivity(@RequestParam("userId") String userId, @RequestParam(name = "lng", required = false, defaultValue = "0") double lng, @RequestParam(name = "lat", required = false, defaultValue = "0") double lat, @RequestParam(name = "range", required = false,defaultValue = "0") double range, ActivityQuery activityQuery) {
         List<CorgiActivity> activityList = corgiActivityService.getCorgiActivityByRange(lng, lat, range, activityQuery);
         List<CorgiActivityDetail> detailList = convertDetail(activityList, userId);
         if (ActivityQuery.SORT_MATCH.equals(activityQuery.getSort())) {
