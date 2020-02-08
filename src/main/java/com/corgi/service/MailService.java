6fd227022cc85fcaba80297bb64b622933958349
tairanliu <dasltr@126.com>
2020-02-08 11:38:49 +0800

    fix bug

--- src/main/java/com/corgi/service/MailService.java
+++ src/main/java/com/corgi/service/MailService.java
@@ -1,10 +1,10 @@
 package com.corgi.service;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.corgi.entity.MailMessage;
 import com.corgi.user.api.CorgiUserService;
 import com.corgi.user.entity.UserDetail;
 import com.sun.mail.util.MailSSLSocketFactory;
-import jdk.nashorn.internal.ir.annotations.Reference;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
