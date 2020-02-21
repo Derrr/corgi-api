5d8fbcc89fef85956b1f27ee6459166b29d8fdf6
tairanliu <dasltr@126.com>
2020-02-21 12:33:03 +0800

    返回报名状态

--- src/main/java/com/corgi/controller/CorgiUserController.java
+++ src/main/java/com/corgi/controller/CorgiUserController.java
@@ -322,7 +322,7 @@ public class CorgiUserController extends BaseController {
     public JsonResult follow(@RequestParam("userId") String userId, @RequestParam("targetUserId") String targetUserId) {
         corgiUserFollowService.follow(userId, targetUserId);
         HashMap extra = new HashMap();
-        extra.put("userId", userId);
+        extra.put("userId", targetUserId);
         mqService.sendMessage(PushMessage.builder()
                 .type(PushMessage.FOLLOW)
                 .sourceUserId(userId)
