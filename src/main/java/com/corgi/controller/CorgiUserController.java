9be3d5cd7fbc8866e5a2694b4b4c35488023167d
tairanliu <dasltr@126.com>
2020-02-22 10:29:22 +0800

    详情返回匹配值

--- src/main/java/com/corgi/controller/CorgiUserController.java
+++ src/main/java/com/corgi/controller/CorgiUserController.java
@@ -202,7 +202,7 @@ public class CorgiUserController extends BaseController {
     @GetMapping("/get_user_detail")
     public JsonResult getUserDetail(@RequestParam("userId") String userId, @RequestParam(name = "loginUserId", required = false) String loginUserId) {
         //dd
-        UserDetail userDetail = corgiUserService.getUserDetail(userId, loginUserId);
+        UserDetail userDetail = corgiUserService.getUserDetail(userId,loginUserId);
         return new JsonResult(userDetail);
     }
 
