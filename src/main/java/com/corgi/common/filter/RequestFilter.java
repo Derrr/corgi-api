8f708a283c5c7168fa329993cf9fb370f6bf2239
tairanliu <dasltr@126.com>
2020-04-30 23:49:56 +0800

    校验用户是否存在

--- src/main/java/com/corgi/common/filter/RequestFilter.java
+++ src/main/java/com/corgi/common/filter/RequestFilter.java
@@ -14,6 +14,8 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.UUID;
 
 @Slf4j
@@ -65,7 +67,48 @@ public class RequestFilter implements Filter {
         MDC.put("reqId", UUID.randomUUID().toString());
         MDC.put("usrID", "");
         filterChain.doFilter(servletRequest, servletResponse);
-        log.info("time spent...{}:{}ms", ((HttpServletRequest) servletRequest).getRequestURI(), (System.currentTimeMillis() - time));
+        String url = ((HttpServletRequest) servletRequest).getRequestURI();
+        String ip = getIpAddr((HttpServletRequest) servletRequest);
+        log.info("{} time spent...{}:{}ms", ip, url, (System.currentTimeMillis() - time));
+    }
+
+    private static final String UNKNOWN = "unknown";
+    private static final String LOCALHOST = "127.0.0.1";
+
+    public static String getIpAddr(HttpServletRequest request) {
+        System.out.println(request);
+        String ipAddress;
+        try {
+            ipAddress = request.getHeader("x-forwarded-for");
+            if (ipAddress == null || ipAddress.length() == 0 || UNKNOWN.equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getHeader("Proxy-Client-IP");
+            }
+            if (ipAddress == null || ipAddress.length() == 0 || UNKNOWN.equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getHeader("WL-Proxy-Client-IP");
+            }
+            if (ipAddress == null || ipAddress.length() == 0 || UNKNOWN.equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getRemoteAddr();
+                if (LOCALHOST.equals(ipAddress)) {
+                    InetAddress inet = null;
+                    try {
+                        inet = InetAddress.getLocalHost();
+                    } catch (UnknownHostException e) {
+                        e.printStackTrace();
+                    }
+                    ipAddress = inet.getHostAddress();
+                }
+            }
+            // 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+            // "***.***.***.***".length()
+            if (ipAddress != null && ipAddress.length() > 15) {
+                if (ipAddress.indexOf(",") > 0) {
+                    ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
+                }
+            }
+        } catch (Exception e) {
+            ipAddress = "";
+        }
+        return ipAddress;
     }
 
     @Override
