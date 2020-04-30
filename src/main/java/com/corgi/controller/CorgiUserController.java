0955ef13bd3c2247158e92db8e867f28f03c8656
tairanliu <dasltr@126.com>
2020-05-01 02:20:38 +0800

    添加ip校验

--- src/main/java/com/corgi/controller/CorgiUserController.java
+++ src/main/java/com/corgi/controller/CorgiUserController.java
@@ -434,11 +434,6 @@ public class CorgiUserController extends BaseController {
         String tel = redisTemplate.opsForValue().get(ipKey);
         if (StringUtils.isNotEmpty(tel) && !tel.equals(telNo)) {
             log.info("duplicate ip..." + ip);
-            try {
-                Thread.sleep(5000L);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
             return new JsonResult();
         }
         redisTemplate.opsForValue().set(ipKey, telNo, 5, TimeUnit.MINUTES);
