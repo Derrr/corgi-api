83255f910624164673e93480fa89d45717c3cd72
tairanliu <dasltr@126.com>
2020-05-15 23:13:59 +0800

    添加日志

--- src/main/java/com/corgi/controller/CorgiToolController.java
+++ src/main/java/com/corgi/controller/CorgiToolController.java
@@ -461,7 +461,7 @@ public class CorgiToolController extends BaseController {
     }
 
     @GetMapping("set_match_ratio")
-    public JsonResult setMatchRatio(HashMap factors) {
+    public JsonResult setMatchRatio(@RequestParam Map factors) {
         log.info("factors..." + factors);
         redisTemplate.opsForHash().putAll(CorgiConstants.MATCH_FACTOR, factors);
         return new JsonResult();
