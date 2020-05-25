68fcdcd8c40c5708d43b11402f6fa6f863eb0fe6
tairanliu <dasltr@126.com>
2020-05-25 10:19:24 +0800

    添加企业发布活动接口

--- src/main/java/com/corgi/controller/CorgiBarController.java
+++ src/main/java/com/corgi/controller/CorgiBarController.java
@@ -32,6 +32,15 @@ public class CorgiBarController extends BaseController {
         return new JsonResult();
     }
 
+    @GetMapping("get_bar_activity")
+    public JsonResult getBarActivity(@RequestParam("barId") String barId,@RequestParam("page")Integer page, @RequestParam("pageSize")Integer pageSize) {
+        CorgiActivity corgiActivity = new CorgiActivity();
+        corgiActivity.setCategory(CorgiActivity.CAT_BUSINESS);
+        corgiActivity.setUserId(barId);
+        List<CorgiActivity> corgiActivities = corgiActivityService.searchCorgiActivity(corgiActivity, page, pageSize);
+        return new JsonResult(corgiActivities);
+    }
+
 
     @PostMapping("add_bar")
     public JsonResult addBar(@RequestBody BarProfile barProfile) {
@@ -40,19 +49,19 @@ public class CorgiBarController extends BaseController {
     }
 
     @PostMapping("update_bar")
-    public JsonResult updateBar(@RequestBody BarProfile barProfile){
+    public JsonResult updateBar(@RequestBody BarProfile barProfile) {
         corgiBarService.updateBarProfile(barProfile);
         return new JsonResult();
     }
 
     @GetMapping("get_bar_list")
-    public JsonResult getBarList(@RequestParam(required = false,name = "status")String status){
+    public JsonResult getBarList(@RequestParam(required = false, name = "status") String status) {
         List<BarProfile> barProfiles = corgiBarService.getBarList(status);
         return new JsonResult(barProfiles);
     }
 
     @GetMapping("get_bar")
-    public JsonResult getBar(@RequestParam(required = false,name = "barId")String barId){
+    public JsonResult getBar(@RequestParam(required = false, name = "barId") String barId) {
         BarProfile barProfiles = corgiBarService.getBarProfile(barId);
         return new JsonResult(barProfiles);
     }
