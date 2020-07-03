8a36eb0e317683b2f4faa2a1ff9f876f3e61c750
tairanliu <dasltr@126.com>
2020-07-03 11:03:58 +0800

    修改获取bar list列表逻辑

--- src/main/java/com/corgi/controller/CorgiBarController.java
+++ src/main/java/com/corgi/controller/CorgiBarController.java
@@ -48,23 +48,23 @@ public class CorgiBarController extends BaseController {
 
     @GetMapping("get_bar_activity")
     public JsonResult getBarActivity(@RequestParam("barId") String barId, @RequestParam("page") Integer page, @RequestParam("pageSize") Integer pageSize) {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-        Date nowDate = new Date();
         CorgiActivity corgiActivity = new CorgiActivity();
         corgiActivity.setCategory(CorgiActivity.CAT_BUSINESS);
         corgiActivity.setUserId(barId);
-        corgiActivity.setEndTime(sdf.format(nowDate));
-        corgiActivity.setStartTime(sdf.format(nowDate));
         List<CorgiActivity> corgiActivities = corgiActivityService.searchCorgiActivity(corgiActivity, page, pageSize);
         return new JsonResult(corgiActivities);
     }
 
     @GetMapping("get_active_bar_activity")
     public JsonResult getActiveBarActivity(@RequestParam("barId") String barId) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        Date nowDate = new Date();
         CorgiActivity corgiActivity = new CorgiActivity();
         corgiActivity.setCategory(CorgiActivity.CAT_BUSINESS);
         corgiActivity.setStatus(CorgiActivity.CREATED);
         corgiActivity.setUserId(barId);
+        corgiActivity.setEndTime(sdf.format(nowDate));
+        corgiActivity.setStartTime(sdf.format(nowDate));
         List<CorgiActivityDetail> corgiActivities = corgiUtilService.convertUserActivityDetail(corgiActivityService.getBarActivity(corgiActivity), getUserId());
         return new JsonResult(corgiActivities);
     }
