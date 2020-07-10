5cab788f5e234fdd9b1ec7e9f79b2d62e554221b
tairanliu <dasltr@126.com>
2020-07-10 15:38:07 +0800

    添加获取榜单接口

--- src/main/java/com/corgi/controller/CorgiBarController.java
+++ src/main/java/com/corgi/controller/CorgiBarController.java
@@ -48,13 +48,9 @@ public class CorgiBarController extends BaseController {
 
     @GetMapping("get_bar_activity")
     public JsonResult getBarActivity(@RequestParam("barId") String barId, @RequestParam("page") Integer page, @RequestParam("pageSize") Integer pageSize) {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-        Date nowDate = new Date();
         CorgiActivity corgiActivity = new CorgiActivity();
         corgiActivity.setCategory(CorgiActivity.CAT_BUSINESS);
         corgiActivity.setUserId(barId);
-        corgiActivity.setEndTime(sdf.format(nowDate));
-        corgiActivity.setStartTime(sdf.format(nowDate));
         List<CorgiActivity> corgiActivities = corgiActivityService.searchCorgiActivity(corgiActivity, page, pageSize);
         return new JsonResult(corgiActivities);
     }
