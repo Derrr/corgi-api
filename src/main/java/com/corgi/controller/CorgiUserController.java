580a6b0a07f76ec9cd7a909ae4fd23838cdb5bb2
tairanliu <dasltr@126.com>
2020-07-30 13:24:33 +0800

    add log

--- src/main/java/com/corgi/controller/CorgiUserController.java
+++ src/main/java/com/corgi/controller/CorgiUserController.java
@@ -26,6 +26,7 @@ import com.corgi.common.util.IPUtil;
 import com.corgi.common.util.JWTUtils;
 import com.corgi.common.util.RequestUtil;
 import com.corgi.entity.CheckPic;
+import com.corgi.entity.CorgiPic;
 import com.corgi.entity.MailMessage;
 import com.corgi.entity.StorageToken;
 import com.corgi.exception.PermissionException;
@@ -318,6 +319,10 @@ public class CorgiUserController extends BaseController {
     public JsonResult getUserDetail(@RequestParam("userId") String userId, @RequestParam(name = "loginUserId", required = false) String loginUserId) {
         try {
             UserDetail userDetail = corgiUserService.getUserDetail(userId, loginUserId);
+            List<UserPic> corgiPics = userDetail.getUserPics();
+            for (UserPic pic : corgiPics) {
+                log.info("pic url... {} ", pic.getPicUrl());
+            }
             if (userDetail == null) {
                 log.info("detail code:{} ", Constants.PARAMETER_ERROR_CODE);
                 return new JsonResult(Constants.PARAMETER_ERROR_CODE, "用户不存在");
