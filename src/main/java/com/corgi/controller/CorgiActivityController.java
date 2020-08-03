e7006e8801adf0b26fb1b1aa96efcf3ae0d9c955
tairanliu <dasltr@126.com>
2020-08-03 19:08:35 +0800

    添加搜索活动接口

--- src/main/java/com/corgi/controller/CorgiActivityController.java
+++ src/main/java/com/corgi/controller/CorgiActivityController.java
@@ -736,6 +736,7 @@ public class CorgiActivityController extends BaseController {
             activityQuery.setPage(1);
         }
         if (hasVersion() && "1.5.0".compareTo(getVersion()) > 0) {
+            activityQuery.setCategory(null);
             List<CorgiActivityDetail> detailList = convertDetail(corgiActivityService.getCorgiActivityByRange(lng, lat, range, activityQuery), userId);
             return new JsonResult(detailList);
         }
