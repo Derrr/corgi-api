6f1759bdc6603d88b59b5c681161e2f64ba2318f
tairanliu <dasltr@126.com>
2020-08-05 19:04:45 +0800

    添加距离时间限制

--- src/main/java/com/corgi/common/filter/RequestFilter.java
+++ src/main/java/com/corgi/common/filter/RequestFilter.java
@@ -60,7 +60,8 @@ public class RequestFilter implements Filter {
         } else if (checkURI(servletRequest, "login")
                 || checkURI(servletRequest, "send_code")
                 || checkURI(servletRequest, "update_user_position")
-                || checkURI(servletRequest, "update_billboard")) {
+                || checkURI(servletRequest, "update_billboard")
+                || checkURI(servletRequest, "clear_influencer")) {
             log.info("into none jwt uri...." + ((HttpServletRequest) servletRequest).getRequestURI());
         }
         MDC.put("reqId", UUID.randomUUID().toString());
