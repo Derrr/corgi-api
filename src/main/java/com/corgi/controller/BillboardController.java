3b1e32ca4c0a690051a40581f03b8934d2b2f194
tairanliu <dasltr@126.com>
2020-09-02 21:32:27 +0800

    添加人脸识别相关接口

---
+++ src/main/java/com/corgi/controller/BillboardController.java
@@ -0,0 +1,32 @@
+package com.corgi.controller;
+
+import com.corgi.common.JsonResult;
+import com.corgi.user.api.CorgiBillboardService;
+import jdk.nashorn.internal.ir.annotations.Reference;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author tairanliu
+ */
+@Slf4j
+@RestController
+@RequestMapping("billboard")
+public class BillboardController extends BaseController {
+    @Reference
+    private CorgiBillboardService corgiBillboardService;
+
+    @GetMapping("get_by_date")
+    public JsonResult getByDate(@RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate) {
+        return new JsonResult(corgiBillboardService.getBillboardByDate(startDate, endDate));
+    }
+
+    @GetMapping("update_by_nickname")
+    public JsonResult updateByNickname(@RequestParam("from") String from, @RequestParam("to") String to, @RequestParam("date") String date) {
+        corgiBillboardService.updateBillboardByNickname(from, to, date);
+        return new JsonResult();
+    }
+}
