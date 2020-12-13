6ad54d7045622c3da0b8409f80041a6c4857f56a
tairanliu <dasltr@126.com>
2020-12-14 01:23:02 +0800

    add log

--- src/main/java/com/corgi/controller/CorgiActivityController.java
+++ src/main/java/com/corgi/controller/CorgiActivityController.java
@@ -1073,6 +1073,11 @@ public class CorgiActivityController extends BaseController {
         CorgiActivity search = new CorgiActivity();
         search.setRefActivityId(activityId);
         List<CorgiActivity> activityList = corgiActivityService.searchCorgiActivity(search, page, size);
+        if (getUserId() == null) {
+            log.info("userId is null");
+        } else {
+            log.info("userId: {}", getUserId());
+        }
         return new JsonResult(convertDetail(activityList, getUserId()));
     }
 
