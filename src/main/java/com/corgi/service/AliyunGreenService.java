0e3e8d5f32db07ea9140939e0dd887ee268a8dae
liutairan <liu.tr@belle.com.cn>
2021-02-01 16:05:27 +0800

    所有活动照片进入照片审核

--- src/main/java/com/corgi/service/AliyunGreenService.java
+++ src/main/java/com/corgi/service/AliyunGreenService.java
@@ -317,7 +317,6 @@ public class AliyunGreenService {
                             if (!suggestion.equals("pass")) {
                                 pic.setStatus(CorgiPic.NEED_CHECK);
                                 pic.setResult(suggestion + "-" + scene + "-" + label + "-" + rate);
-                                addCheckPic(pic, sourceId, type);
                                 needCheck = true;
                                 break;
                             }
@@ -326,6 +325,7 @@ public class AliyunGreenService {
                             pic.setStatus(CorgiPic.NORMAL);
                             pic.setResult("pass");
                         }
+                        addCheckPic(pic, sourceId, type);
                     } else {
                         String result = "task process fail. task response:" + JSON.toJSONString(taskResult);
                         pic.setStatus(CorgiPic.NEED_CHECK);
