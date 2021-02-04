e7f81f2a1a3cfff8667e0cd3bcf998c3eb425e2d
liutairan <liu.tr@belle.com.cn>
2021-02-04 10:50:42 +0800

    添加获取视频信息接口

---
+++ src/main/java/com/corgi/service/AliyunVodService.java
@@ -0,0 +1,51 @@
+package com.corgi.service;
+
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.profile.DefaultProfile;
+import com.aliyuncs.profile.IClientProfile;
+import com.aliyuncs.vod.model.v20170321.GetPlayInfoRequest;
+import com.aliyuncs.vod.model.v20170321.GetPlayInfoResponse;
+import com.corgi.common.JsonResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author tairanliu
+ */
+@Slf4j
+@Service
+public class AliyunVodService {
+
+    @Value("${aliyun.accessKeyId}")
+    private String accessKeyId;
+
+    @Value("${aliyun.AccessKeySecret}")
+    private String accessKeySecret;
+    private IAcsClient managementClient;
+    String REGION_ID = "cn-shanghai";
+
+    @PostConstruct
+    void init() {
+        IClientProfile profile = DefaultProfile.getProfile(REGION_ID, accessKeyId, accessKeySecret);
+        this.managementClient = new DefaultAcsClient(profile);
+    }
+
+    @GetMapping("get_video_info")
+    public JsonResult getVideoUrl(@RequestParam("videoId") String videoId) {
+        GetPlayInfoRequest request = new GetPlayInfoRequest();
+        request.setVideoId(videoId);
+        try {
+            return new JsonResult(this.managementClient.getAcsResponse(request));
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
+        return new JsonResult();
+    }
+}
