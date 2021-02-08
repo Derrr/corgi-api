9297dff93514943e66eb27700ab09956616ea1a6
liutairan <liu.tr@belle.com.cn>
2021-02-08 10:38:58 +0800

    fix bug

--- src/main/java/com/corgi/controller/CorgiActivityController.java
+++ src/main/java/com/corgi/controller/CorgiActivityController.java
@@ -281,7 +281,7 @@ public class CorgiActivityController extends BaseController {
             return new JsonResult(Constants.PARAMETER_ERROR_CODE, "点赞失败，活动不存在");
         }
         CorgiActivity activity = activityList.get(0);
-        log.info(""+activity);
+        log.info("" + activity);
         activityLike.setUserId(activity.getUserId());
         if (hasUserId()) {
             activityLike.setLikeUserId(getUserId());
@@ -299,7 +299,7 @@ public class CorgiActivityController extends BaseController {
         HashMap extra = new HashMap();
         extra.put("activityId", activityLike.getActivityId());
         extra.put("type", PushMessage.LIKE_COMMENT_TYPE);
-        if (!activityLike.getUserId().equals(activityLike.getLikeUserId())) {
+        if (!activityLike.getLikeUserId().equals(activityLike.getUserId())) {
             mqService.sendMessage(PushMessage.builder()
                     .type(PushMessage.DEFAULT)
                     .sourceUserId(activityLike.getLikeUserId())
