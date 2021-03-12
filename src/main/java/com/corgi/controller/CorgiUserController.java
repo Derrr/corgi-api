1ce4169ce166f724a11afade38adc4b660d04e84
liutairan <liu.tr@belle.com.cn>
2021-03-12 10:39:13 +0800

    fix bug

--- src/main/java/com/corgi/controller/CorgiUserController.java
+++ src/main/java/com/corgi/controller/CorgiUserController.java
@@ -438,6 +438,10 @@ public class CorgiUserController extends BaseController {
                 } else if (!userPosition.getUserId().equals(jwtUserId)) {
                     throw new PermissionException(Constants.PERMISSION_ERROR_CODE, "非当前用户");
                 } else {
+                    UserDetail u = corgiUserService.getUserDetailBasic(jwtUserId);
+                    if(u == null || StringUtils.isEmpty(u.getUserId())){
+                        throw new PermissionException(Constants.PERMISSION_ERROR_CODE, "用户不存在");
+                    }
                     Date expireDate = decodedJWT.getExpiresAt();
                     if (expireDate.getTime() - System.currentTimeMillis() < JWTUtils.expireTime) {
                         result.put("jwt", JWTUtils.createJWT(jwtUserId, userPosition.getVersion()));
