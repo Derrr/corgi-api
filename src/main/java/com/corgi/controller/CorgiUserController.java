db170be1be1ecf56f32609482324e3510d1e08e2
liutairan <liu.tr@belle.com.cn>
2021-03-18 10:08:45 +0800

    delete log

--- src/main/java/com/corgi/controller/CorgiUserController.java
+++ src/main/java/com/corgi/controller/CorgiUserController.java
@@ -384,7 +384,6 @@ public class CorgiUserController extends BaseController {
                 log.info("detail code:{} ", Constants.PARAMETER_ERROR_CODE);
                 return new JsonResult(Constants.PARAMETER_ERROR_CODE, "用户不存在");
             }
-            log.info("detail code:{} ", 0);
             return new JsonResult(userDetail);
         } catch (Exception e) {
             log.error(e.getMessage(), e);
