55aa54f43299b497e0cd74ce30d86a8c3d9d2b11
liutairan <liu.tr@belle.com.cn>
2021-03-20 12:58:26 +0800

    fix bug

--- src/main/java/com/corgi/service/AliyunVodService.java
+++ src/main/java/com/corgi/service/AliyunVodService.java
@@ -39,7 +39,6 @@ public class AliyunVodService {
     public GetPlayInfoResponse getVideoUrl(String videoId) {
         GetPlayInfoRequest request = new GetPlayInfoRequest();
         request.setVideoId(videoId);
-        request.setFormats("mp4");
         try {
             log.info("preparing request info...");
             GetPlayInfoResponse response = this.managementClient.getAcsResponse(request);
