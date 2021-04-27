279f1e3f51470b091eefecda997541166501c2d1
liutairan <liu.tr@belle.com.cn>
2021-04-27 11:03:49 +0800

    fix bug

--- src/main/java/com/corgi/controller/CorgiFeedController.java
+++ src/main/java/com/corgi/controller/CorgiFeedController.java
@@ -268,7 +268,7 @@ public class CorgiFeedController extends BaseController {
             Iterator<CorgiActivity> it = activityList.iterator();
             while (it.hasNext()) {
                 CorgiActivity activity = it.next();
-                if (!userId.equals(activity.getUserId()) && "fail".equals(activity.getCheckStatus())) {
+                if (StringUtils.isEmpty(activity.getUserId()) || (!userId.equals(activity.getUserId()) && "fail".equals(activity.getCheckStatus()))) {
                     it.remove();
                     continue;
                 }
