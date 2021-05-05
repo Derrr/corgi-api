ed82eee8023f97eefe9092b1e190be6296b9285f
liutairan <liu.tr@belle.com.cn>
2021-05-06 00:26:24 +0800

    fix bug

--- src/main/java/com/corgi/common/filter/RequestFilter.java
+++ src/main/java/com/corgi/common/filter/RequestFilter.java
@@ -41,7 +41,7 @@ public class RequestFilter implements Filter {
             try {
                 decodedJWT = JWTUtils.verifyToken(jwt);
             } catch (Exception e) {
-                log.error(e.getMessage());
+                //log.error(e.getMessage());
                 JsonResult jsonResult = new JsonResult("");
                 jsonResult.setCode(Constants.JWT_ERROR_CODE);
                 jsonResult.setMessage(e.getMessage());
