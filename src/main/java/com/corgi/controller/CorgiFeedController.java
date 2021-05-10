c7bf53732a068f6f74352418d4bff08d76c07620
liutairan <liu.tr@belle.com.cn>
2021-05-10 23:16:09 +0800

    添加待评价接口

--- src/main/java/com/corgi/controller/CorgiFeedController.java
+++ src/main/java/com/corgi/controller/CorgiFeedController.java
@@ -273,10 +273,10 @@ public class CorgiFeedController extends BaseController {
             Iterator<CorgiActivity> it = activityList.iterator();
             while (it.hasNext()) {
                 CorgiActivity activity = it.next();
-                if ("2021/02/07".compareTo(activity.getCreateTime()) > 0) {
-                    it.remove();
-                    continue;
-                }
+//                if ("2021/02/07".compareTo(activity.getCreateTime()) > 0) {
+//                    it.remove();
+//                    continue;
+//                }
                 if (StringUtils.isEmpty(activity.getUserId()) || (!userId.equals(activity.getUserId()) && "fail".equals(activity.getCheckStatus()))) {
                     it.remove();
                     continue;
