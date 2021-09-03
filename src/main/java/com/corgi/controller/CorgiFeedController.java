0fda5df3f6abfa64a9efbdeac41d56001414d1f9
liutairan <liu.tr@belle.com.cn>
2021-09-03 16:51:41 +0800

    add log

--- src/main/java/com/corgi/controller/CorgiFeedController.java
+++ src/main/java/com/corgi/controller/CorgiFeedController.java
@@ -438,13 +438,13 @@ public class CorgiFeedController extends BaseController {
             }
             i++;
         }
-        if (!added) {
-            userActivities.add(activity);
+        if (userActivities.size() < size) {
+            if (!added) {
+                userActivities.add(activity);
+            }
+            return userActivities;
         }
-        if (userActivities.size() > size) {
-            return userActivities.subList(0, size);
-        }
-        return userActivities;
+        return userActivities.subList(0, size);
     }
 
     private VlogDetail getVlogDetail(String activityId, String userId) {
