3937b1ecfb295840cb7031f4712868e4efb80635
liutairan <liu.tr@belle.com.cn>
2021-10-12 19:02:07 +0800

    去除新人推送

--- src/main/java/com/corgi/controller/CorgiToolController.java
+++ src/main/java/com/corgi/controller/CorgiToolController.java
@@ -185,6 +185,7 @@ public class CorgiToolController extends BaseController {
     @PostMapping("update_activity_topic")
     public JsonResult upadteActivityTopic(@RequestBody CorgiActivityDetail corgiActivity) {
         corgiToolService.updateActivityTopic(corgiActivity.getActivityId(), corgiActivity.getTopics());
+        corgiActivityService.updateByColumnn(corgiActivity.getActivityId(), "topics", corgiActivity.getTopics().get(0));
         return new JsonResult();
     }
 
