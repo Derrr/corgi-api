27e52b3f05c09439b948d1b8bdcd32dba947734b
liutairan <liu.tr@belle.com.cn>
2021-12-05 11:47:31 +0800

    fix bug

--- src/main/java/com/corgi/controller/CorgiActivityController.java
+++ src/main/java/com/corgi/controller/CorgiActivityController.java
@@ -372,6 +372,7 @@ public class CorgiActivityController extends BaseController {
                 .merchId(merchId)
                 .build();
         String marketId = corgiOrderService.addUserMarket(market);
+        log.info("marketId:{} ", marketId);
         activity.setMarketId(marketId);
         if (!StringUtils.isEmpty(activity.getVideoId())) {
             CorgiVlog corgiVlog = new CorgiVlog();
