ee94de811628ccad99d83a25549ad9734a958716
liutairan <liu.tr@belle.com.cn>
2021-12-09 16:47:01 +0800

    fix bug

--- src/main/java/com/corgi/controller/CorgiActivityController.java
+++ src/main/java/com/corgi/controller/CorgiActivityController.java
@@ -1095,6 +1095,7 @@ public class CorgiActivityController extends BaseController {
         query.setGoodsType(CorgiUserGoods.GOODS_TYPE.ACTIVITY);
         query.setStart((page - 1) * pageSize);
         query.setSize(pageSize);
+        log.info("goods:{}", query);
         List<CorgiUserGoods> goods = corgiOrderService.getUserGoods(query);
         if (CollectionUtils.isEmpty(goods)) {
             return new JsonResult(new ArrayList<>());
