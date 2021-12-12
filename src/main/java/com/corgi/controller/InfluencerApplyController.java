111233e678325775abe5b6f19e703782f7e90db2
liutairan <liu.tr@belle.com.cn>
2021-12-12 12:42:11 +0800

    fix bug

--- src/main/java/com/corgi/controller/InfluencerApplyController.java
+++ src/main/java/com/corgi/controller/InfluencerApplyController.java
@@ -1,6 +1,7 @@
 package com.corgi.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.fastjson.JSONObject;
 import com.corgi.activity.api.CorgiActivityService;
 import com.corgi.activity.entity.CorgiActivity;
 import com.corgi.common.JsonResult;
@@ -13,14 +14,12 @@ import com.corgi.user.entity.ActivityBillboard;
 import com.corgi.user.entity.InfluencerApply;
 import com.corgi.user.entity.UserProfile;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author tairanliu
@@ -33,14 +32,30 @@ public class InfluencerApplyController extends BaseController {
     private CorgiInfluencerApplyService corgiInfluencerApplyService;
 
     @PostMapping("apply")
-    public JsonResult apply(@RequestBody InfluencerApply apply) {
+    public JsonResult apply(@RequestBody JSONObject content) {
+        InfluencerApply apply = new InfluencerApply();
+        apply.setContent(content.toJSONString());
         corgiInfluencerApplyService.addApply(apply);
         return new JsonResult();
     }
 
     @GetMapping("get_applies")
     public JsonResult getApplies(@RequestParam("page") Integer page, @RequestParam("size") Integer size) {
-        return new JsonResult(corgiInfluencerApplyService.getApplies(page, size));
+        List<InfluencerApply> applies = corgiInfluencerApplyService.getApplies(page, size);
+        List<JSONObject> result = new ArrayList<>();
+        if (CollectionUtils.isEmpty(applies)) {
+            for (InfluencerApply apply : applies) {
+                if (StringUtils.isEmpty(apply.getContent())) {
+                    continue;
+                }
+                try {
+                    result.add(JSONObject.parseObject(apply.getContent()));
+                } catch (Exception e) {
+                    log.error("apply:{} ", apply.getContent());
+                }
+            }
+        }
+        return new JsonResult(result);
     }
 
 
