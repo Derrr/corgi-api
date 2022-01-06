b75c5ac51bc19b54437f7dbadd99900500947fdc
liutairan <liu.tr@belle.com.cn>
2022-01-06 13:27:26 +0800

    fix bug

--- src/main/java/com/corgi/service/AliyunDypnsService.java
+++ src/main/java/com/corgi/service/AliyunDypnsService.java
@@ -75,7 +75,7 @@ public class AliyunDypnsService {
                 .setExpire(900L)
                 .setSmsCodeExpire(300);
         getSmsAuthTokensRequest.setSmsTemplateCode("SMS_232165801");
-        getSmsAuthTokensRequest.setSignName(request.getSignName());
+        getSmsAuthTokensRequest.setSignName("Corgi");
         // 复制代码运行请自行打印 API 的返回值
         GetSmsAuthTokensResponse response = managementClient.getSmsAuthTokens(getSmsAuthTokensRequest);
         return response.getBody();
