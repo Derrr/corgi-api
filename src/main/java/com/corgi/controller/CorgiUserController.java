b7fbd7e9c7ebaf954037029f5a31970927a4524b
liutairan <liu.tr@belle.com.cn>
2022-02-26 12:46:13 +0800

    fix bug

--- src/main/java/com/corgi/controller/CorgiUserController.java
+++ src/main/java/com/corgi/controller/CorgiUserController.java
@@ -1123,7 +1123,8 @@ public class CorgiUserController extends BaseController {
         extra.put("type", "907");
         JSONArray content = new JSONArray();
         UserDetail detail = corgiUserService.getUserDetailBasic(getUserId());
-        content.add(new JSONObject().fluentPut("text", "@" + detail.getNickname()).fluentPut("url", detail.getUserId()).fluentPut("urlType", "4"));
+        content.add(new JSONObject().fluentPut("text", detail.getNickname()).fluentPut("url", detail.getUserId())
+                .fluentPut("urlType", "4").fluentPut("isBold", true));
         content.add(new JSONObject().fluentPut("text", " 想看到你发布付费动态"));
         extra.put("content", content);
         extra.put("bottomText", "去发布>");
