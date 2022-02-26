43f2809771309c4335fa3a2c722fa2c18a933a28
liutairan <liu.tr@belle.com.cn>
2022-02-26 15:06:01 +0800

    fix bug

--- src/main/java/com/corgi/controller/CorgiFeedController.java
+++ src/main/java/com/corgi/controller/CorgiFeedController.java
@@ -108,15 +108,15 @@ public class CorgiFeedController extends BaseController {
             List<CorgiActivity> corgiActivities = corgiActivityService.getActivityByIds(feedIds);
             List<CorgiActivityDetail> details = convertDetail(corgiActivities, userId);
             String version = getVersion();
-            if (!StringUtils.isEmpty(version)
-                    && version.replaceAll("android", "").compareTo("2.1.0") < 0) {
-                for (String feed : feedIds) {
-                    CorgiFeed view = new CorgiFeed();
-                    view.setUserId(userId);
-                    view.setFeed(feed);
-                    corgiFeedService.viewFeed(view);
-                }
+//            if (!StringUtils.isEmpty(version)
+//                    && version.replaceAll("android", "").compareTo("2.1.0") < 0) {
+            for (String feed : feedIds) {
+                CorgiFeed view = new CorgiFeed();
+                view.setUserId(userId);
+                view.setFeed(feed);
+                corgiFeedService.viewFeed(view);
             }
+//            }
             if (!isNew) {
                 mqService.refreshFeed(userId);
             }
