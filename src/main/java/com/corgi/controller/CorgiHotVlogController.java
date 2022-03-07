39bd5eea655db3c3c23dbb0b04c64fb0ad42e1f4
liutairan <liu.tr@belle.com.cn>
2022-03-08 00:41:20 +0800

    fix bug

--- src/main/java/com/corgi/controller/CorgiHotVlogController.java
+++ src/main/java/com/corgi/controller/CorgiHotVlogController.java
@@ -174,9 +174,9 @@ public class CorgiHotVlogController extends BaseController {
             if (CollectionUtils.isNotEmpty(pics)) {
                 extra.put("picUrl", pics.get(0).getPicUrl());
             }
-            extra.put("desc", "你关注的好友 " + detail.getNickname() + " 发布的付费动态正在被围观快去看看吧！");
-        } else {
             extra.put("desc", "你关注的好友 " + detail.getNickname() + " 发布的动态正在被围观快去看看吧！");
+        } else {
+            extra.put("desc", "你关注的好友 " + detail.getNickname() + " 发布的付费动态正在被围观快去看看吧！");
             extra.put("showPayReadMask", true);
         }
         pushMessage.setExtra(extra);
