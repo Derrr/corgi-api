874ae88be526c1ab704f15269b73ae78f44c9735
liutairan <liu.tr@belle.com.cn>
2022-05-15 22:05:46 +0800

    fix bug

--- src/main/java/com/corgi/controller/CorgiToolController.java
+++ src/main/java/com/corgi/controller/CorgiToolController.java
@@ -87,10 +87,14 @@ public class CorgiToolController extends BaseController {
         config.put("showPay", 1);
         config.put("showDiscovery", 1);
         config.put("showPurse", 1);
+        config.put("showMap", 1);
         if ("meizu".equals(RequestUtil.getChannel())) {
             config.put("showPay", 0);
             config.put("showDiscovery", 0);
             config.put("showPurse", 0);
+        }
+        if ("huawei".equals(RequestUtil.getChannel())) {
+            config.put("showMap", 0);
         }
         return new JsonResult(config);
     }
