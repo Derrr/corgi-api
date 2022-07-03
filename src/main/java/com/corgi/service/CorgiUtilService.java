171b4d5c6c617b175bfaee7b70d37748317242c2
liutairan <liu.tr@belle.com.cn>
2022-07-03 21:23:27 +0800

    fix bug

--- src/main/java/com/corgi/service/CorgiUtilService.java
+++ src/main/java/com/corgi/service/CorgiUtilService.java
@@ -60,7 +60,7 @@ public class CorgiUtilService {
     private CorgiBarService corgiBarService;
     @Reference
     private CorgiPicService corgiPicService;
-    public static final List<String> CHANNELS = Arrays.asList("oppo", "xiaomi", "huawei");
+    public static final List<String> CHANNELS = Arrays.asList("xiaomi", "huawei");
 
     private ThreadLocal<String> value = new ThreadLocal<>();
 
