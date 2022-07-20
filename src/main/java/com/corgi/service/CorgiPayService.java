8d70ba42c328e482199c989df40f0e6bcc92cd90
liutairan <liu.tr@belle.com.cn>
2022-07-20 12:48:00 +0800

    fix bug

--- src/main/java/com/corgi/service/CorgiPayService.java
+++ src/main/java/com/corgi/service/CorgiPayService.java
@@ -111,10 +111,14 @@ public class CorgiPayService {
 
     public Map<String, String> wxRefundOrder(CorgiOrder order) throws Exception {
         Map<String, String> orderMap = new HashMap<>();
+        String refundFee = new Double(order.getPayAmount() * 100).intValue() + "";
         orderMap.put("out_trade_no", order.getTradeNo());
+        orderMap.put("out_refund_no", "r" + order.getTradeNo());
+        orderMap.put("total_fee", refundFee);
+        orderMap.put("refund_fee", refundFee);
         Map<String, String> result = wxPay.refund(orderMap);
         order.setResult("订单退款");
-        order.setStatus(CorgiOrder.STATUS.CLOSE);
+        order.setStatus(CorgiOrder.STATUS.PROCESSING);
         return result;
     }
 
@@ -135,7 +139,6 @@ public class CorgiPayService {
         body.put("out_trade_no", order.getTradeNo());
         body.put("notify_url", "https://api.corgi.org.cn/order/wx_callback");
         body.put("total_fee", (long) (merchandise.getPrice() * 100) + "");
-        //body.put("spbill_create_ip", "123.12.12.12");
         body.put("trade_type", "APP");
         try {
             Map<String, String> response = wxPay.unifiedOrder(body);
@@ -146,8 +149,6 @@ public class CorgiPayService {
             result.put("noncestr", response.get("nonce_str"));
             result.put("prepayid", response.get("prepay_id"));
             result.put("package", "Sign=WXPay");
-            //result.put("signType", "MD5");
-            //result.put("sign", response.get("sign"));
             result.put("sign", WXPayUtil.generateSignature(result, CorgiWXPayConfig.config.getKey()));
             return result;
         } catch (Exception e) {
