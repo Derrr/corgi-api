1c2cb881490e856a8ae99d6a38ae4ea874232085
liutairan <liu.tr@belle.com.cn>
2022-07-25 12:33:25 +0800

    fix bug

--- src/main/java/com/corgi/controller/CorgiFeedController.java
+++ src/main/java/com/corgi/controller/CorgiFeedController.java
@@ -519,6 +519,9 @@ public class CorgiFeedController extends BaseController {
 
     private VlogDetail buildVlogDetail(CorgiVlog vlog, String userId) {
         CorgiActivity activity = corgiActivityFeedService.getActivityById(vlog.getActivityId());
+        if (Arrays.asList("check", "not_good", "fail").contains(activity.getCheckStatus())) {
+            return null;
+        }
         VlogDetail vlogDetail = VlogDetail.createDetail(vlog);
         vlogDetail.setUserDetail(corgiUserService.getUserDetailBasic(vlog.getUserId()));
         vlogDetail.setActivityDetail(activity);
