60c19b07f69302706d22f11545918ed48b0c2b79
liutairan <liu.tr@belle.com.cn>
2022-07-26 11:30:13 +0800

    fix bug

--- src/main/java/com/corgi/controller/CorgiUserController.java
+++ src/main/java/com/corgi/controller/CorgiUserController.java
@@ -1060,6 +1060,7 @@ public class CorgiUserController extends BaseController {
     @GetMapping("get_map_user")
     public JsonResult getMapUser(UserQuery userQuery) {
         userQuery.setUserId(getUserId());
+        log.info("userQuery:{} ", userQuery);
         MapUserProfile mapUserProfile = corgiUserService.getMapUser(userQuery);
         return new JsonResult(mapUserProfile);
     }
