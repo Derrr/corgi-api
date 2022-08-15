0df0f17350cb91441293d1d81c51af70630dfb09
liutairan <liu.tr@belle.com.cn>
2022-08-15 19:41:28 +0800

    fix bug

--- src/main/java/com/corgi/controller/CorgiMatchController.java
+++ src/main/java/com/corgi/controller/CorgiMatchController.java
@@ -121,44 +121,43 @@ public class CorgiMatchController extends BaseController {
                     }
                 }
                 result = remain - matchIds.size();
-//                if (result < 0) {
-//                    return new JsonResult(Constants.API_ERROR_CODE, "用户速配次数不足");
-//                }
-                extra.put("type", PushMessage.QUICK_MATCH_TYPE);
-//                if (!CollectionUtils.isEmpty(remains) && !CollectionUtils.isEmpty(matchIds)) {
-//
-//                    List<String> lastMatchList = redisTemplate.opsForList().range(matchKey, 0, -1);
-//                    redisTemplate.delete(matchKey);
-//
-//
-//                    if (lastMatchList == null) {
-//                        lastMatchList = new ArrayList<>();
-//                    }
-                int i = 0;
-//                    for (UserMatchRemain remain1 : remains) {
-//                        Integer size = remain1.getRemain();
-                int size = 100;
-                for (int j = size; j > 0; j--) {
-                    if (i >= matchIds.size()) {
-                        return new JsonResult(result);
-                    }
-                    String matchId = matchIds.get(i);
-//                            redisTemplate.opsForList().rightPush(matchKey, matchId);
-//                            if (lastMatchList.contains(matchId)) {
-//                                continue;
-//                            }
-//                            corgiUserMatchService.addUserMatch(getUserId(), matchId, remain1.getTradeNo());
-                    mqService.sendMessage(PushMessage.builder()
-                            .type(PushMessage.DEFAULT)
-                            .sourceUserId(getUserId())
-                            .targetUserId(matchId)
-                            .message("有一个小哥哥想和你匹配，要去聊聊吗？")
-                            .extra(extra)
-                            .build());
-                    i++;
+                if (result < 0) {
+                    return new JsonResult(Constants.API_ERROR_CODE, "用户速配次数不足");
                 }
-//                    }
-//                }
+                extra.put("type", PushMessage.QUICK_MATCH_TYPE);
+                if (!CollectionUtils.isEmpty(remains) && !CollectionUtils.isEmpty(matchIds)) {
+
+                    List<String> lastMatchList = redisTemplate.opsForList().range(matchKey, 0, -1);
+                    redisTemplate.delete(matchKey);
+
+
+                    if (lastMatchList == null) {
+                        lastMatchList = new ArrayList<>();
+                    }
+                    int i = 0;
+                    for (UserMatchRemain remain1 : remains) {
+                        Integer size = remain1.getRemain();
+                        for (int j = size; j > 0; j--) {
+                            if (i >= matchIds.size()) {
+                                return new JsonResult(result);
+                            }
+                            String matchId = matchIds.get(i);
+                            redisTemplate.opsForList().rightPush(matchKey, matchId);
+                            if (lastMatchList.contains(matchId)) {
+                                continue;
+                            }
+                            corgiUserMatchService.addUserMatch(getUserId(), matchId, remain1.getTradeNo());
+                            mqService.sendMessage(PushMessage.builder()
+                                    .type(PushMessage.DEFAULT)
+                                    .sourceUserId(getUserId())
+                                    .targetUserId(matchId)
+                                    .message("有一个小哥哥想和你匹配，要去聊聊吗？")
+                                    .extra(extra)
+                                    .build());
+                            i++;
+                        }
+                    }
+                }
             }
         } finally {
             if (redisTemplate.hasKey(key)) {
