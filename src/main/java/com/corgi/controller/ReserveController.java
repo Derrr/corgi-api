213cdaba910c279249b9471ec5d90a354294f66f
liutairan <liu.tr@belle.com.cn>
2022-08-16 15:52:01 +0800

    fix bug

--- src/main/java/com/corgi/controller/ReserveController.java
+++ src/main/java/com/corgi/controller/ReserveController.java
@@ -8,8 +8,12 @@ import com.corgi.user.entity.*;
 import com.corgi.user.enums.MerchandiseEnum;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author tairanliu
@@ -33,7 +37,22 @@ public class ReserveController extends BaseController {
 
     @GetMapping("list_reservations")
     public JsonResult listReservation(BarReservation reservation, @RequestParam("page") Integer page, @RequestParam("size") Integer size) {
-        return new JsonResult(corgiReserveService.listAllReservation(reservation, (page - 1) * size, size));
+        List<BarReservation> reservationList = corgiReserveService.listAllReservation(reservation, (page - 1) * size, size);
+        List<CorgiBarReservation> result = new ArrayList<>();
+        if (!CollectionUtils.isEmpty(reservationList)) {
+            for (BarReservation reserve : reservationList) {
+                CorgiBarReservation corgiBarReservation = new CorgiBarReservation();
+                BeanUtils.copyProperties(reserve, corgiBarReservation);
+                CorgiOrder order = corgiOrderService.getOrderByTradeNo(reserve.getTradeNo());
+                if (order != null) {
+                    corgiBarReservation.setPayAmount(order.getPayAmount());
+                    corgiBarReservation.setPayTime(order.getCtime());
+                }
+                corgiBarReservation.setBarProfile(corgiBarService.getBarProfile(reserve.getBarId()));
+                result.add(corgiBarReservation);
+            }
+        }
+        return new JsonResult(result);
     }
 
     @GetMapping("count_reservation")
@@ -56,7 +75,7 @@ public class ReserveController extends BaseController {
                 BarReservation reservation = corgiReserveService.getReservationById(order.getMarketId());
                 if (reservation != null) {
                     CorgiBarReservation corgiBarReservation = new CorgiBarReservation();
-                    BeanUtils.copyProperties(reservation,corgiBarReservation);
+                    BeanUtils.copyProperties(reservation, corgiBarReservation);
                     corgiBarReservation.setPayAmount(order.getPayAmount());
                     corgiBarReservation.setBarProfile(corgiBarService.getBarProfile(reservation.getBarId()));
                 }
--- src/main/java/com/corgi/entity/CorgiBarReservation.java
+++ src/main/java/com/corgi/entity/CorgiBarReservation.java
@@ -2,7 +2,6 @@ package com.corgi.entity;
 
 import com.corgi.user.entity.BarProfile;
 import com.corgi.user.entity.BarReservation;
-import com.corgi.user.entity.CorgiOrder;
 import lombok.Data;
 
 
@@ -10,4 +9,5 @@ import lombok.Data;
 public class CorgiBarReservation extends BarReservation {
     private BarProfile barProfile;
     private Double payAmount;
+    private String payTime;
 }
