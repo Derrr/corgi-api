7ece08f0b3c3049a5c4701e308bc1eb01c766c27
liutairan <liu.tr@belle.com.cn>
2022-08-18 11:23:26 +0800

    fix bug

--- src/main/java/com/corgi/controller/CorgiUserController.java
+++ src/main/java/com/corgi/controller/CorgiUserController.java
@@ -634,13 +634,14 @@ public class CorgiUserController extends BaseController {
             corgiUserService.updateUserPosition(userPosition);
         }
         if (redisTemplate.opsForValue().setIfAbsent("update_user_" + userPosition.getUserId(), "1", 5L, TimeUnit.MINUTES)) {
-            log.info("updating...from url");
             UserDetail userDetail = corgiUserService.getUserDetailBasic(userPosition.getUserId());
-            userDetail.setTime(System.currentTimeMillis());
-            if(!"influencer".equals(userDetail.getAvatarStatus())){
-                userDetail.setAvatarStatus(corgiUserService.getUserVipExpire(userDetail.getUserId()));
+            if (userDetail != null) {
+                userDetail.setTime(System.currentTimeMillis());
+                if (!"influencer".equals(userDetail.getAvatarStatus())) {
+                    userDetail.setAvatarStatus(corgiUserService.getUserVipExpire(userDetail.getUserId()));
+                }
+                corgiMatchService.updateUser(userDetail);
             }
-            corgiMatchService.updateUser(userDetail);
         }
         String expireDate = corgiUserService.getUserVipExpire(userPosition.getUserId());
         CorgiUserVipDetail detail = new CorgiUserVipDetail();
