7b5375f65edfcef8247c6e437f8438fcc7967405
liutairan <liu.tr@belle.com.cn>
2022-09-14 17:21:34 +0800

    fix bug

--- src/main/java/com/corgi/controller/CorgiUserController.java
+++ src/main/java/com/corgi/controller/CorgiUserController.java
@@ -595,11 +595,13 @@ public class CorgiUserController extends BaseController {
                 if (JWTUtils.ADMIN_ID.equals(jwtUserId)) {
                     result.put("jwt", JWTUtils.createJWT(userPosition.getUserId(), userPosition.getVersion()));
                 } else if (!userPosition.getUserId().equals(jwtUserId)) {
-                    throw new PermissionException(Constants.PERMISSION_ERROR_CODE, "非当前用户");
+                    log.error("非当前用户:" + jwtUserId + " v:" + userPosition.getVersion());
+                    return new JsonResult(Constants.PERMISSION_ERROR_CODE,"非当前用户");
                 } else {
                     UserLogin u = corgiUserService.getUserLogin(jwtUserId);
                     if (u == null || StringUtils.isEmpty(u.getUserId())) {
-                        throw new PermissionException(Constants.PERMISSION_ERROR_CODE, "用户不存在:" + jwtUserId + " v:" + userPosition.getVersion());
+                        log.error("用户不存在:" + jwtUserId + " v:" + userPosition.getVersion());
+                        return new JsonResult(Constants.PERMISSION_ERROR_CODE,"用户不存在");
                     }
                     Date expireDate = decodedJWT.getExpiresAt();
                     if (expireDate.getTime() - System.currentTimeMillis() < JWTUtils.expireTime) {
@@ -620,8 +622,6 @@ public class CorgiUserController extends BaseController {
                             .build());
                 }
             }
-        } catch (PermissionException e) {
-            throw new PermissionException(Constants.PERMISSION_ERROR_CODE, e.getMessage());
         } catch (Exception e) {
             log.error(e.getMessage(), e);
             throw new PermissionException(Constants.SERVER_ERROR_CODE, e.getMessage());
