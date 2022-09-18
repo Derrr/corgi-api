3d3cbf0460be0955a0e8f7ed5af0c2f785aa0ba3
liutairan <liu.tr@belle.com.cn>
2022-09-18 16:34:21 +0800

    fix bug

--- src/main/java/com/corgi/controller/CorgiOrderController.java
+++ src/main/java/com/corgi/controller/CorgiOrderController.java
@@ -519,7 +519,7 @@ public class CorgiOrderController extends BaseController {
                             CorgiMerchandise lastMerchandise = new CorgiMerchandise();
                             for (CorgiMerchandise merchandise : merchandises) {
                                 lastMerchandise = merchandise;
-                                if (merchandise.getPrice().equals(merchId)) {
+                                if (merchandise.getId().equals(merchId)) {
                                     break;
                                 }
                             }
