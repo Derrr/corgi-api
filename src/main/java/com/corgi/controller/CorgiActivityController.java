191576edb6b6892fa4d4995130b1f0e2a78dd659
liutairan <liu.tr@belle.com.cn>
2022-09-22 18:33:14 +0800

    fix bug

--- src/main/java/com/corgi/controller/CorgiActivityController.java
+++ src/main/java/com/corgi/controller/CorgiActivityController.java
@@ -1196,12 +1196,13 @@ public class CorgiActivityController extends BaseController {
                 String key = "heat_topic_" + topic + "-" + query.getCity() + page + "-" + size;
                 activityIds = redisTemplate.opsForList().range(key, 0, -1);
                 if (CollectionUtils.isEmpty(activityIds)) {
-                    activityIds = corgiUserActivityService.getHeatActivity(query, page, size);
+
                     redisTemplate.opsForList().rightPushAll(key, activityIds);
                     redisTemplate.expire(key, 20l, TimeUnit.HOURS);
                 }
                 if ("64".equals(topic)) {
                     redisTemplate.delete(key);
+                    activityIds = corgiUserActivityService.getHeatActivity(query, page, size);
                     redisTemplate.opsForList().rightPushAll(key, activityIds);
                     redisTemplate.expire(key, 1l, TimeUnit.MINUTES);
                 }
