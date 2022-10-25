6ecb84c24e748e9365eca336a92e90cabd2aaf90
trliu <trliu@hillinsight.com>
2022-10-25 12:12:45 +0800

    fix bug

--- src/main/java/com/corgi/controller/CorgiUserController.java
+++ src/main/java/com/corgi/controller/CorgiUserController.java
@@ -125,7 +125,7 @@ public class CorgiUserController extends BaseController {
     public JsonResult register(@RequestBody UserLogin userLogin) {
         String lockKey = "login_" + userLogin.getTelNo();
         String code = redisTemplate.opsForValue().get(CODE_PREFIX + userLogin.getTelNo());
-        if ((code != null && code.equals(userLogin.getCode())) || ("00000".equals(userLogin.getCode()) && ("13700000000".equals(userLogin.getTelNo())) || "99999999999".equals(userLogin.getTelNo()))))
+        if ((code != null && code.equals(userLogin.getCode())) || ("00000".equals(userLogin.getCode()) && ("13700000000".equals(userLogin.getTelNo()) || "99999999999".equals(userLogin.getTelNo()))))
         {
             try {
                 corgiUtilService.lock(lockKey);
