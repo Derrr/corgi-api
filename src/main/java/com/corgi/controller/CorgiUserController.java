545c47a8b5651db91d2772496cc48af7c50b1d4e
trliu <trliu@hillinsight.com>
2023-03-20 18:48:46 +0800

    添加风险注册校验

--- src/main/java/com/corgi/controller/CorgiUserController.java
+++ src/main/java/com/corgi/controller/CorgiUserController.java
@@ -1103,7 +1103,7 @@ public class CorgiUserController extends BaseController {
     }
 
     @GetMapping("get_map_user")
-    public JsonResult getMapUser(Map userQueryMap) {
+    public JsonResult getMapUser(@RequestParam Map userQueryMap) {
         String userQueryJson = JSONObject.toJSONString(userQueryMap);
         log.info("map:{} ", userQueryJson);
         UserQuery userQuery = JSONObject.parseObject(userQueryJson, UserQuery.class);
