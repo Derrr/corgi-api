5be6140bac418509ba40dfd83c2a51f139cc2892
trliu <trliu@hillinsight.com>
2023-06-15 13:51:54 +0800

    添加风险注册校验

--- src/main/java/com/corgi/entity/UserExtraResult.java
+++ src/main/java/com/corgi/entity/UserExtraResult.java
@@ -1,6 +1,7 @@
 package com.corgi.entity;
 
 import com.alibaba.fastjson.JSONArray;
+import com.corgi.user.entity.UserExtra;
 import lombok.Data;
 import org.springframework.util.StringUtils;
 
@@ -14,28 +15,28 @@ public class UserExtraResult {
         super();
     }
 
-//    public UserExtraResult(UserExtra userExtra) {
-//        this.profession = userExtra.getProfession();
-//        this.aim = userExtra.getAim();
-//        this.income = userExtra.getIncome();
-//        this.education = userExtra.getEducation();
-//        this.xp = userExtra.getXp();
-//        try {
-//            if (!StringUtils.isEmpty(userExtra.getInterests())) {
-//                this.interests = JSONArray.parseArray(userExtra.getInterests(), String.class);
-//            }
-//        } catch (Exception e) {
-//            this.interests = Arrays.asList(userExtra.getInterests().split(","));
-//        }
-//        try {
-//            if (!StringUtils.isEmpty(userExtra.getTags())) {
-//                this.tags = JSONArray.parseArray(userExtra.getTags(), String.class);
-//            }
-//        } catch (Exception e) {
-//            this.interests = Arrays.asList(userExtra.getTags().split(","));
-//        }
-//
-//    }
+    public UserExtraResult(UserExtra userExtra) {
+        this.profession = userExtra.getProfession();
+        this.aim = userExtra.getAim();
+        this.income = userExtra.getIncome();
+        this.education = userExtra.getEducation();
+        this.xp = userExtra.getXp();
+        try {
+            if (!StringUtils.isEmpty(userExtra.getInterests())) {
+                this.interests = JSONArray.parseArray(userExtra.getInterests(), String.class);
+            }
+        } catch (Exception e) {
+            this.interests = Arrays.asList(userExtra.getInterests().split(","));
+        }
+        try {
+            if (!StringUtils.isEmpty(userExtra.getTags())) {
+                this.tags = JSONArray.parseArray(userExtra.getTags(), String.class);
+            }
+        } catch (Exception e) {
+            this.interests = Arrays.asList(userExtra.getTags().split(","));
+        }
+
+    }
 
     private String profession;
     private String income;
