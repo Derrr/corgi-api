2503142446e0cbd0be27ad31c527a267c91deb8b
trliu <trliu@hillinsight.com>
2023-06-28 18:39:36 +0800

    添加风险注册校验

--- src/main/java/com/corgi/entity/UserExtraResult.java
+++ src/main/java/com/corgi/entity/UserExtraResult.java
@@ -20,28 +20,15 @@ public class UserExtraResult {
         this.aim = userExtra.getAim();
         this.income = userExtra.getIncome();
         this.education = userExtra.getEducation();
-        try {
-            if (!StringUtils.isEmpty(userExtra.getXp())) {
-                this.xp = JSONArray.parseArray(userExtra.getXp(), String.class);
-            }
-        } catch (Exception e) {
-            this.xp = Arrays.asList(userExtra.getInterests().split(","));
+        if (!StringUtils.isEmpty(userExtra.getXp())) {
+            this.xp = Arrays.asList(userExtra.getXp().split(","));
         }
-        try {
-            if (!StringUtils.isEmpty(userExtra.getInterests())) {
-                this.interests = JSONArray.parseArray(userExtra.getInterests(), String.class);
-            }
-        } catch (Exception e) {
+        if (!StringUtils.isEmpty(userExtra.getInterests())) {
             this.interests = Arrays.asList(userExtra.getInterests().split(","));
         }
-        try {
-            if (!StringUtils.isEmpty(userExtra.getTags())) {
-                this.tags = JSONArray.parseArray(userExtra.getTags(), String.class);
-            }
-        } catch (Exception e) {
-            this.interests = Arrays.asList(userExtra.getTags().split(","));
+        if (!StringUtils.isEmpty(userExtra.getTags())) {
+            this.tags = Arrays.asList(userExtra.getTags().split(","));
         }
-
     }
 
     private String profession;
