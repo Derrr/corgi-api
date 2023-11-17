1ea203e4bd6cd78f8605dd9e5b55a9220ffb5dcb
trliu <trliu@hillinsight.com>
2023-11-17 23:32:04 +0800

    添加extra信息隐藏

--- src/main/java/com/corgi/controller/CorgiUserController.java
+++ src/main/java/com/corgi/controller/CorgiUserController.java
@@ -1198,7 +1198,7 @@ public class CorgiUserController extends BaseController {
         String[] ids = userIds.split(",");
         List<UserMatchProfile> profiles = new ArrayList<>();
         Long threshold = System.currentTimeMillis() - 2 * 60000;
-        if (ids.length < 50) {
+        if (ids.length < 30) {
             for (int i = 0; i < ids.length; i++) {
                 UserDetail userDetail = corgiUserService.getUserDetailBasic(ids[i]);
                 if (userDetail != null) {
