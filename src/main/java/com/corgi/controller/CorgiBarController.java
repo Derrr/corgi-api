5a1f544666d8700a730bc2b22007b40f03787ca3
trliu <trliu@hillinsight.com>
2023-11-25 22:01:33 +0800

    添加extra信息隐藏

--- src/main/java/com/corgi/controller/CorgiBarController.java
+++ src/main/java/com/corgi/controller/CorgiBarController.java
@@ -100,6 +100,7 @@ public class CorgiBarController extends BaseController {
         CorgiActivity corgiActivity = new CorgiActivity();
         corgiActivity.setCategory(CorgiActivity.CAT_BUSINESS);
         corgiActivity.setUserId(barId);
+        corgiActivity.setStatus(CorgiActivity.NOT_DELETED);
         if (!StringUtils.isEmpty(status)) {
             corgiActivity.setStatus(status);
         }
