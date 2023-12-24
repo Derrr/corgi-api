656c6adba96836c31aff4515827e5a7f9c752089
trliu <trliu@hillinsight.com>
2023-12-24 13:32:26 +0800

    添加extra信息隐藏

--- src/main/java/com/corgi/service/AliyunGreenService.java
+++ src/main/java/com/corgi/service/AliyunGreenService.java
@@ -71,7 +71,7 @@ public class AliyunGreenService {
     public static String FAIL = "fail";
     public static String NOT_GOOD = "not_good";
     public static List<String> CHECK_LIST = Arrays.asList("check", "not_good", "fail");
-    public static List<String> BLOCK_WORD_LIST = Arrays.asList("飞机杯", "打飞机", "初一", "初二", "初三",
+    public static List<String> BLOCK_WORD_LIST = Arrays.asList("飞机杯", "打飞机", "初一", "初二", "初三", "男高", "粗口"
             "高一", "高二", "高三", "初中", "高中", "小学生", "sao0", "sao的", "骚的", "奴", "贱", "革命", "起义", "嫖", "娼",
             "无毛", "按摩", "有偿", "技师", "捆绑", "调教", "学生党", "求C", "约p", "约P", "可飞", "乳头",
             "精液", "色情", "原味", "无套");
