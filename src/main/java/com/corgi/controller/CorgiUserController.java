e18462eb35b5f729e2630f1bed9f5a845b6461fc
trliu <trliu@hillinsight.com>
2024-03-27 11:07:11 +0800

    添加微信解锁相关接口

--- src/main/java/com/corgi/controller/CorgiUserController.java
+++ src/main/java/com/corgi/controller/CorgiUserController.java
@@ -380,8 +380,8 @@ public class CorgiUserController extends BaseController {
 
     @PostMapping("/update_nickname")
     public JsonResult updateNickname(@RequestBody UserDetail userDetail) throws PermissionException{
-        if (!hasUserId()) {
-            throw new PermissionException(Constants.API_ERROR_CODE, "无权限操作");
+        if (!hasUserId() ) {
+            return new JsonResult();
         }
         userDetail.setUserId(getUserId());
         if (StringUtils.isEmpty(userDetail.getUserId())) {
