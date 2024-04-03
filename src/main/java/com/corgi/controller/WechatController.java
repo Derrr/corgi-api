214f0a20dcd6a16bac3432a361faad4d137ba683
trliu <trliu@hillinsight.com>
2024-04-03 14:24:04 +0800

    添加微信解锁相关接口

--- src/main/java/com/corgi/controller/WechatController.java
+++ src/main/java/com/corgi/controller/WechatController.java
@@ -2,18 +2,13 @@ package com.corgi.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.corgi.common.JsonResult;
-import com.corgi.common.constant.Constants;
 import com.corgi.user.api.*;
-import com.corgi.user.entity.UserDetail;
-import com.corgi.user.entity.UserProfile;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 /**
  * @author tairanliu
@@ -29,17 +24,17 @@ public class WechatController extends BaseController {
 
     @GetMapping("bind")
     public JsonResult bind(@RequestParam("wechatId") String wechatId, @RequestParam("corgiTel") String corgiTel) {
-        if (!"-2".equals(getUserId())) {
-            return new JsonResult();
-        }
-        UserDetail detail = new UserDetail();
-        detail.setTelNo(corgiTel);
-        List<UserProfile> profiles = corgiUserService.searchUsers(detail, null, 1, 1);
-        if (CollectionUtils.isEmpty(profiles) || profiles.get(0) == null) {
-            return new JsonResult(Constants.API_ERROR_CODE, "用户不存在");
-        }
-        corgiToolService.bindWechat(wechatId, profiles.get(0).getUserId());
-        return new JsonResult(profiles.get(0).getUserId());
+//        if (!"-2".equals(getUserId())) {
+//            return new JsonResult();
+//        }
+//        UserDetail detail = new UserDetail();
+//        detail.setTelNo(corgiTel);
+//        List<UserProfile> profiles = corgiUserService.searchUsers(detail, null, 1, 1);
+//        if (CollectionUtils.isEmpty(profiles) || profiles.get(0) == null) {
+//            return new JsonResult(Constants.API_ERROR_CODE, "用户不存在");
+//        }
+//        corgiToolService.bindWechat(wechatId, profiles.get(0).getUserId());
+//        return new JsonResult(profiles.get(0).getUserId());
     }
 
     @GetMapping("getCorgiId")
