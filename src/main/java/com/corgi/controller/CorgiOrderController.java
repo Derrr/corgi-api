085d87a3591753687b319fd39a52e293a0e6e5ef
trliu <trliu@hillinsight.com>
2024-04-11 22:40:43 +0800

    添加微信解锁相关接口

--- src/main/java/com/corgi/controller/CorgiOrderController.java
+++ src/main/java/com/corgi/controller/CorgiOrderController.java
@@ -609,6 +609,7 @@ public class CorgiOrderController extends BaseController {
         //}
         JSONObject result = corgiPayService.verifyApplePay(receiptData, password);
         order.setResult(result.toJSONString());
+        log.info("result:{}", order.getResult());
         if ("0".equals(result.getString("status"))) {
             order.setStatus(CorgiOrder.STATUS.SUCCESS);
         } else {
