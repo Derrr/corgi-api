e1b183e06a8bb17da3e104242e7bed0d4ca812e8
trliu <trliu@hillinsight.com>
2024-07-29 14:56:48 +0800

    添加微信解锁相关接口

--- src/main/java/com/corgi/controller/CorgiOrderController.java
+++ src/main/java/com/corgi/controller/CorgiOrderController.java
@@ -831,6 +831,7 @@ public class CorgiOrderController extends BaseController {
         result.put("totalWithdraw", totalWithdraw);
         result.put("balancePay", balancePay);
         result.put("remainWithdraw", (totalIncome - balancePay) * rate - totalWithdraw);
+        result.put("remainBalance", totalIncome - balancePay - totalWithdraw);
         return new JsonResult(result);
     }
 
