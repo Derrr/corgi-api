1931d60d4f10aa13c24c0d4ec7d59dad1f12b9e1
trliu <trliu@hillinsight.com>
2024-08-23 19:12:29 +0800

    升级内容安全接口

--- src/main/java/com/corgi/common/filter/RequestFilter.java
+++ src/main/java/com/corgi/common/filter/RequestFilter.java
@@ -81,6 +81,7 @@ public class RequestFilter implements Filter {
                 || checkURI(servletRequest, "Socket")
                 || checkURI(servletRequest, "chat")
                 || checkURI(servletRequest, "get_config")
+                || checkURI(servletRequest, "get_version")
                 || checkURI(servletRequest, "yinyuan")
                 || checkURI(servletRequest, "callback")) {
             log.info("into none jwt uri...." + ((HttpServletRequest) servletRequest).getRequestURI());
