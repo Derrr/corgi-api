570a73c2df299bbe176f65c660a1526e54b11b7e
trliu <trliu@hillinsight.com>
2024-12-24 15:20:37 +0800

    升级内容安全接口

--- src/main/java/com/corgi/controller/CorgiToolController.java
+++ src/main/java/com/corgi/controller/CorgiToolController.java
@@ -1008,7 +1008,7 @@ public class CorgiToolController extends BaseController {
             tlxActivity.setId(obj.getString("ID"));
             tlxActivity.setCity(obj.getString("city"));
             tlxActivity.setBody(obj.getString("body"));
-            tlxActivity.setDays(obj.getDouble("days"));
+            tlxActivity.setDays(obj.getString("days"));
             tlxActivity.setPeriod(obj.getString("period"));
             tlxActivity.setDepartdate(obj.getString("departdate"));
             tlxActivity.setExpenseDetail(obj.getString("expense_detail"));
@@ -1017,7 +1017,7 @@ public class CorgiToolController extends BaseController {
             tlxActivity.setMeetingPoint(obj.getString("meeting_point"));
             tlxActivity.setNote(obj.getString("note"));
             tlxActivity.setPosterImage(obj.getString("poster_image"));
-            tlxActivity.setPrice(obj.getDouble("price"));
+            tlxActivity.setPrice(obj.getString("price"));
             tlxActivity.setShorttitle(obj.getString("shorttitle"));
             tlxActivity.setTripContent(obj.getString("trip_content"));
             tlxActivity.setVersion(version);
